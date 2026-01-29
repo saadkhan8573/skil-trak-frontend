49b0254728bb09262c0fb64ae4cbb661a988db65
saadkhan8573 <saad.h4896@gmail.com>
2026-01-29 09:30:04 +0500

    asdasd

--- partials/rto-v2/industry-detail/components/GenerateIndustryBioModal.tsx
+++ partials/rto-v2/industry-detail/components/GenerateIndustryBioModal.tsx
@@ -1,4 +1,3 @@
-
 import { Button, ShowErrorNotifications } from '@components'
 import {
     Dialog,
@@ -32,7 +31,6 @@ export function GenerateIndustryBioModal({
         setGeneratedBio('')
         try {
             const res = await generateBio(industryId).unwrap()
-            console.log({ res })
             // Assuming the API returns { data: "generated bio string" } or similar
             // Adjust based on actual API response structure
             setGeneratedBio(res?.bio)

3ec60b3a2acc9af07263c180872f39146306a5cf
Developer <dev@example.com>
2026-03-03 08:26:43 +0500

    Build Error Fixed

--- partials/rto-v2/placement-request-detail/PlacementRequestDetail.tsx
+++ partials/rto-v2/placement-request-detail/PlacementRequestDetail.tsx
@@ -101,7 +101,7 @@ export const PlacementRequestDetail = () => {
             ? providedWorkplaceStages
             : needsWorkplaceStages
     const progressData = RtoV2Api.PlacementRequests.useStudentPlacementProgress(
-        wpId!,
+        Number(wpId),
         {
             skip: !wpId,
         }
@@ -132,7 +132,7 @@ export const PlacementRequestDetail = () => {
         const index = progressData?.data?.findIndex(
             (s: any) => s.stage === currentStatus
         )
-        return index !== -1 ? index : 0
+        return typeof index === 'number' && index !== -1 ? index : 0
     }
 
     const studentPreferences = [
